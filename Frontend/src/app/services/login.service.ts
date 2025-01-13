a972b22d4f93e041a8f480e37ad610563943f11b
DouglasMaceddo <dougasdv@gmail.com>
2025-01-13 10:51:03 -0300

    adding functions

--- Frontend/src/app/services/login.service.ts
+++ Frontend/src/app/services/login.service.ts
@@ -1,35 +1,50 @@
-  import { HttpClient } from '@angular/common/http';
-  import { Injectable } from '@angular/core';
-  import { LoginResponse } from '../types/login-response.type';
-  import { tap } from 'rxjs';
-  import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class LoginService {
-    apiUrl: string = "http://localhost:8080/auth"
 
-    constructor(private httpClient: HttpClient, private router: Router) { }
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth"
 
-    login(email: string, password: string){
-      return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
-        tap((value) => {
-          sessionStorage.setItem("auth-token", value.token)
-        })
-      )
-    }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
-    signup(name: string, email: string, password: string){
-      return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
-        tap((value) => {
-          sessionStorage.setItem("auth-token", value.token)
-        })
-      )
-    }
-    
-    logout(): void {
-      sessionStorage.removeItem('auth-token');
-      this.router.navigate(['/Login']);
+  login(email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+      })
+    )
+  }
+
+  logout(): void {
+    sessionStorage.removeItem('auth-token');
+    this.router.navigate(['/Login']);
+  }
+
+  isAdmin(): boolean {
+    const token = sessionStorage.getItem('auth-token');
+    if (!token) return false;
+
+    try {
+      const decodedToken: any = jwtDecode(token);
+      return decodedToken.role === 'admin';
+    } catch (e) {
+      console.error('Erro ao decodificar o token:', e);
+      return false;
     }
   }
+}
