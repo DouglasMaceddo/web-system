72720c77df2eab821fd7f8f294a41ff2598fed96
DouglasMaceddo <dougasdv@gmail.com>
2025-01-15 16:54:12 -0300

    commit

--- Backend/src/main/java/com/example/SistemaRoupas/entity/Produto.java
+++ Backend/src/main/java/com/example/SistemaRoupas/entity/Produto.java
@@ -5,9 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Value;
 
 @Data
 @Entity
+
 public class Produto {
 
     @Id
