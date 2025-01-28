0c095668a0153e64b02408d9ecba425a8771ec7a
DouglasMaceddo <dougasdv@gmail.com>
2025-01-28 11:59:39 -0300

    adicionando funcionalidades

--- Backend/src/main/java/com/example/SistemaRoupas/controllers/UserController.java
+++ Backend/src/main/java/com/example/SistemaRoupas/controllers/UserController.java
@@ -1,15 +1,15 @@
 package com.example.SistemaRoupas.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     @GetMapping
     public ResponseEntity<String> getUser(){
         return ResponseEntity.ok("sucesso!");
     }
+
 }
